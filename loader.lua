6ac08649b4e5abcc4ae1b09ffb2723afe20bae92
seikoso <75853142+seikosos@users.noreply.github.com>
2025-06-30 12:48:38 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,5 @@
 game.StarterGui:SetCore("SendNotification", {Title="old loadstring!"; Text="please use the new loadstring (copied to clipboard)"; Duration=3;})
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamNullFire/NullFire/main/loader.lua"))()
-
 pcall(function()
   setclipboard([[loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamNullFire/NullFire/main/loader.lua"))()]])
   end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamNullFire/NullFire/main/loader.lua"))()
